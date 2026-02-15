b58de7faf043ad17aeb20d29f79f945e1ee62f3f
Emily Pagliarini <emily.pagliarini@snhu.edu>
2026-02-15 16:49:55 -0500

    select screen

---
+++ godotions/start stuff/select_screen.gd
@@ -0,0 +1,31 @@
+extends Control
+
+var p1_controller = null
+var p2_controller = null
+
+@onready var p1_text := $Player1Bg/Player1JoinText
+@onready var p2_text := $Player2Bg/Player2JoinText
+
+@onready var animation_player := $AnimationPlayer
+
+signal players_joined(p1_controller, p2_controller)
+
+func _ready() -> void:
+	animation_player.play("pull_up_animation")
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventJoypadButton:
+		var device := event.device
+		if p1_controller == null:
+			p1_controller = device
+			p1_text.text = "WELCOME PLAYER 1"
+			
+		if p2_controller == null and device != p1_controller:
+			p2_controller = device
+			p2_text.text = "WELCOME PLAYER 2"
+			emit_signal("players_joined",p1_controller,p2_controller)
+	
+	
+func _process(delta: float) -> void:
+	p1_text.text = "[wave amp=60 freq=3]PRESS ANY BUTTON TO JOIN[/wave]"
+	p2_text.text = "[wave amp=60 freq=3]PRESS ANY BUTTON TO JOIN[/wave]"
