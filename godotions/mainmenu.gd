70ad0dbeb44c2360bee784fa5c2abc2065266798
Emily Pagliarini <emily.pagliarini@snhu.edu>
2026-02-22 14:32:43 -0500

    updated title scene

--- godotions/mainmenu.gd
+++ godotions/mainmenu.gd
@@ -1,13 +1,14 @@
 extends Control
 
-func _on_play_pressed() -> void:
-	get_tree().change_scene_to_file("res://start stuff/select_screen.tscn")
-	#change this to whatever scene is the first one. When i tested the mainhideout.tscn
-	#was first so I dont want to break anything just change it here lol.
-
-
 func _on_options_pressed() -> void: #do we want options menu?
 	pass # Replace with function body.
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
+
+func _on_start_pressed() -> void:
+	var loading_scene = preload("res://start stuff/select_screen.tscn")
+	var loading = loading_scene.instantiate()
+	add_child(loading)
+	#change this to whatever scene is the first one. When i tested the mainhideout.tscn
+	#was first so I dont want to break anything just change it here lol.
