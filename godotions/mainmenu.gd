34fe0dfa408a5443a2333b800f477d3f90db4ee2
Madhav Patel <madhav.patel@snhu.edu>
2026-02-22 15:57:46 -0500

    i tried

--- godotions/mainmenu.gd
+++ godotions/mainmenu.gd
@@ -4,8 +4,8 @@ func _on_quit_pressed() -> void:
 	get_tree().quit()
 
 func _on_start_pressed() -> void:
-	var loading_scene = preload("res://start stuff/select_screen.tscn")
+	var loading_scene = load("res://mainmap.tscn")
 	var loading = loading_scene.instantiate()
-	add_child(loading)
+	#add_child(loading)
 	#change this to whatever scene is the first one. When i tested the mainhideout.tscn
 	#was first so I dont want to break anything just change it here lol.
